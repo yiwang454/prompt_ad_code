071a6639f01e11b6e8d1f1f9b3179e70ab5f79c3
yiwang454 <yiwang454@gmail.com>
2023-03-31 10:59:52 +0800

    running scripts cleaning

--- run_prompt_finetune_test.py
+++ run_prompt_finetune_test.py
@@ -8,7 +8,12 @@ GPU_idx = 0
 COMMAND_LIST = []
 for template_id in [1, 3]:
     for seed in [1, 2, 10, 18, 26, 31, 32, 52, 61, 68, 70, 72, 85, 93, 94]:
-        command = '''python prompt_ad/prompt_finetune.py \
+        command = '''python prompt_ad_code/prompt_finetune.py \
+                --project_root /parent/directory/of_prompt_ad_code \
+                --logs_root /directory/to/store/your/output \
+                --off_line_model_dir /directory/you/store/pre-trained/model/from/huggingface \
+                --data_dir /directory/you/store/ADReSS/data \
+                 --data_not_saved \           
                 --seed {:d} \
                 --tune_plm \
                 --model bert \
@@ -18,7 +23,6 @@ for template_id in [1, 3]:
                 --gpu_num {} \
                 --num_epochs 10 \
                 --no_ckpt True \
-                --no_tensorboard \
                 --last_ckpt \
                 --no_tensorboard'''.format(seed, template_id, GPU_nums[GPU_idx], asr_form)
         COMMAND_LIST.append(command)
